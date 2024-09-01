87365e9e816ab8fb96c8de683d846af1e214fe8a
Azizeh <azizeh.sheykhalishahi@gmail.com>
2024-09-01 08:03:56 +0330

    Fixed #7 by using drf_yasg as OpenAPI epecification and Updateing README and requirments files

--- ads_manager/settings.py
+++ ads_manager/settings.py
@@ -39,12 +39,20 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # Build-in Django apps
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+
+    # Third-party apps
+    'rest_framework',
+    'drf_yasg', # OpenAPI documentation
+
+    # Custom apps
+    
 ]
 
 MIDDLEWARE = [
